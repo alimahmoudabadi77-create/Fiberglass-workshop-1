91ee3afa1632b98b4646b8580bc02c698dec8afe
alimahmoudabadi77-create <alimahmoudabadi77@gmail.com>
2025-12-07 23:05:46 +0330

    edit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',  // غیرفعال برای توسعه محلی
+  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  // basePath: '/Fiberglass-workshop',  // غیرفعال برای توسعه محلی
-  // assetPrefix: '/Fiberglass-workshop/',  // غیرفعال برای توسعه محلی
-  // اجازه صفحات داینامیک که client-side رندر می‌شوند
+  basePath: '/Fiberglass-workshop',
+  assetPrefix: '/Fiberglass-workshop/',
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
